a9733d33a9a27f358bbc5b3d1933de792fa40755
Luis Ángel Vega Rodríguez <vrl4@outlook.es>
2022-03-16 20:23:14 -0600

    feat: adding login page style

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,31 @@
 <template>
   <div id="app">
-    <nav>
-      <ul>
-        <li>
-        <router-link  to="/consults">Consults</router-link>
-        <router-link  to="/insert">Insert</router-link>
-        <router-link to="/">Login</router-link>
-        </li>
-      </ul>
-    </nav>
+    <b-navbar toggleable="lg" type="dark" variant="primary">
+    <b-container>
+    <b-navbar-brand to="/consults">ObsidanCode</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item to="/consults">Consults</b-nav-item>
+        <b-nav-item to="/insert">Insert</b-nav-item>
+      </b-navbar-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item-dropdown right>
+          <!-- Using 'button-content' slot -->
+          <template #button-content>
+            <em>User</em>
+          </template>
+          <b-dropdown-item to="/">Sign Out</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+    </b-container>
+  </b-navbar>
+
     <router-view></router-view>
   </div>
 </template>
@@ -26,14 +43,3 @@ export default {
   },
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,57 +1,85 @@
-<template lang="html">
-<div class="container">
-   <div class="card card-container">
-       <h1>Login</h1>
-       <form @submit.prevent="checkCredentials">
-           <input type="text" v-model="credentials.userName" id="inputUserName" placeholder="User Name" required autofocus>
-           <input type="password" v-model="credentials.password" id="inputPassword" placeholder="Password" required>
-           <button>Login</button>
-       </form>
-           
-    </div>
-</div>
+<template>
+  <div>
+    <b-container>
+      <b-row class="justify-content-center mt-4">
+        <b-col col md="4">
+          <b-card
+          title="Sign In"
+          bg-variant="light">
+              <b-form @submit.prevent="checkCredentials">
 
+                <b-form-group
+                  id="input-group-1"
+                  label="User Name:"
+                  label-for="input-1"
+                  label-align="left"
+                >
+                  <b-form-input
+                    id="input-1"
+                    v-model="credentials.userName"
+                    type="text"
+                    placeholder="Enter User Name"
+                  ></b-form-input>
+                </b-form-group>
+                <b-form-group
+                  id="input-group-2"
+                  label="Password:"
+                  label-for="input-2"
+                  label-align="left"
+                >
+                  <b-form-input
+                    id="input-2"
+                    v-model="credentials.password"
+                    type="password"
+                    placeholder="Enter password"
+                    required
+                  >
+                  </b-form-input>
+                </b-form-group>
+
+                <b-button type="submit" variant="primary">Sign In</b-button>
+              </b-form>
+          </b-card>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <script>
-    class credentials{
-        constructor(userName, password) {
-            this.userName = userName, 
-            this.password = password
-            
-        }
-    }
+class credentials {
+  constructor(userName, password) {
+    (this.userName = userName), (this.password = password);
+  }
+}
 
 export default {
-    data() {
-        return {
-            credentials: new credentials()
-        }
-    },
-    methods: {
-        checkCredentials() {
-            fetch('http://localhost:3000/login', {
-                method: "POST",
-                body: JSON.stringify(this.credentials),
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                }
-            }).then(res => res.json())
-            .then(function(data) {
-                localStorage.setItem('logged', data.access);
-                if (data.access != true) {
-                    window.alert("Invalid credentials")
-                }
-            })
-            this.$router.push('/consults')
-            this.credentials = new credentials()
+  data() {
+    return {
+      credentials: new credentials(),
+    };
+  },
+  methods: {
+    checkCredentials() {
+      fetch("http://localhost:3000/login", {
+        method: "POST",
+        body: JSON.stringify(this.credentials),
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
         },
-    }
-
+      })
+        .then((res) => res.json())
+        .then(function (data) {
+          if (data.access != true) {
+            window.alert("Invalid credentials");
+          } else {
+            localStorage.setItem("logged", data.access);
+          }
+        });
+      this.$router.push("/consults");
+      this.credentials = new credentials();
+    },
+  },
 };
 </script>
-
-<style lang="css">
-
-</style>
